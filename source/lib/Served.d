b4b1fc529d0b5838a03f7cba269c06030dc07a3e
unknown <mit@mail.bg>
2015-10-02 16:48:20 +0300

    first

--- source/lib/Served.d
+++ source/lib/Served.d
@@ -3,8 +3,42 @@ import std.stdio;
 public import CRouter;
 
 class Served{
+	private CRouter _router;
+	
 	this(){
 		writeln("Welcome to Served constructor");
+	}
+	
+	private void init(){
+	}
+	
+	private void defaultConfiguration(){
+	}
+	
+	private void lazyRouter(){
+		if (!this._router){
+			this._router = new CRouter();
+		}
+	}
+	
+	private void handle(){
+		if(!_router){
+			writeln("Router not set up!");
+			return;
+		}
+		//_router.handle();
+	}
+	
+	
+	public Served use(){
+		
+		return this;
+	}
+	
+	public Object route(string path){
+		this.lazyRouter();
+		
+		return new Object();
 	}
 	
 }
