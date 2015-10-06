703b7461bf0ebc44dc305ca9d094cdbad747878f
unknown <mit@mail.bg>
2015-10-06 16:55:52 +0300

    multiple connections

--- source/lib/_http_server.d
+++ source/lib/_http_server.d
@@ -3,11 +3,14 @@ import std.socket;
 import std.conv;
 import std.string;
 import std.file;
+import std.array;
 
 string CRLF = "\r\n";
 
 class CHTTPServer{
 	private Socket _socket;
+	
+	private int _maxConnections = 60;
 	
 	this(){
 	}
@@ -57,21 +60,111 @@ class CHTTPServer{
 	}
 	
 	void handleHTTP(){
+		Socket[] sockets;
 		Socket currSock;
 		uint bytesRead;
 		ubyte[4096] buff;
-	 
+		
+		/*
 		while (true) {
 			currSock = _socket.accept();
 			
 			while ((bytesRead = currSock.receive(buff)) > 0){
-				string request = cast(string)buff[0..bytesRead];
+				string request = cast(string)buff[0..bytesRead-1];
 				parseRequestAndRespond(request, currSock);
 			}	
 				
 			currSock.close();
 			buff.destroy();
 		}
+		*/
+		
+		auto sset = new SocketSet(_maxConnections + 1);
+		
+		for (;; sset.reset()) {
+			sset.add(_socket);
+			foreach (each; sockets)	sset.add(each);
+			
+			// Update socket set with only those sockets that have data
+			// avaliable for reading. Options are for read, write,
+			// and error.
+			Socket.select(sset, null, null);
+			
+			// Read the data from each socket remaining, and handle the request.
+			
+			for (int i = 0; ; i++) {
+NEXT:
+				if (i == sockets.length) break;
+				
+				if (sset.isSet(sockets[i])) {
+					int read = sockets[i].receive(buff);
+					
+					if (Socket.ERROR == read) {
+						debug writeln("Connection error.");
+						goto SOCK_DOWN;
+					} else if (read == 0) {
+						debug {
+							try {
+								// If the connection closed due to an error, remoteAddress() could fail.
+								writefln("Connection from %s closed.", sockets[i].remoteAddress().toString());
+							} catch (SocketException) {
+								writeln("Connection closed.");
+							}
+						}
+SOCK_DOWN:
+						sockets[i].close(); //Release socket resources now.
+ 
+						// Remove from socket from sockets, and id from threads.
+						if (i != sockets.length - 1) sockets[i] = sockets.back;
+						
+						sockets.length--;
+						
+						debug writeln("\tTotal connections: ", sockets.length);
+						goto NEXT; // -i- is still the NEXT index.
+					} else {
+						debug
+							writefln("Received %d bytes from %s:"
+									 ~ "\n-----",
+									 read, sockets[i].remoteAddress().toString());
+ 
+						//Handle the request
+						string request = cast(string)buff[0..read-1];
+						parseRequestAndRespond(request, sockets[i]);
+						//sockets[i].send(buf[0 .. read]);
+						writeln("");
+					}
+				}	
+			}
+			
+			// Connection request.
+			if (sset.isSet(_socket)) {
+				Socket sn;
+				try {
+					if (sockets.length < _maxConnections) {
+						sn = _socket.accept();
+						debug writefln("Connection from %s established.", sn.remoteAddress().toString());
+						assert(sn.isAlive);
+						assert(_socket.isAlive);
+						sockets ~= sn;
+						debug writefln("\tTotal connections: %d", sockets.length);
+					} else {
+						sn = _socket.accept();
+						debug writefln("Rejected connection from %s;"
+									   ~ " too many connections.",
+									   sn.remoteAddress().toString());
+						assert(sn.isAlive);
+						sn.close();
+						assert(!sn.isAlive);
+						assert(_socket.isAlive);
+					}
+				} catch (Exception e) {
+					debug writefln("Error accepting: %s", e.toString());
+					if(sn) sn.close();
+				}
+			}
+			
+		}
+		
 	}
 	
 	void listen(ushort port = 80){
